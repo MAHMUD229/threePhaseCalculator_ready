67c3ceda262163534fcf6148b900b7c1d8dd4735
Sergey <duna.scs.u@gmail.com>
2023-09-14 22:39:41 +0300

    add "Phase" processing to parseXml function

--- functions.cpp
+++ functions.cpp
@@ -45,6 +45,82 @@ QMap<QString, Phase> parseXml(const QString &filePath, long double &voltage, QSt
                     return phasesMap;
                 }
             }
+
+            // Обработка элемента "Phase"
+            if (elementName == "Phase") {
+                QString phaseName = xmlReader.attributes().value("name").toString();
+
+                // Создание новой фазы, если ее еще нет в карте
+                if (!phasesMap.contains(phaseName)) {
+                    phasesMap[phaseName] = Phase();
+                }
+
+                Phase &phase = phasesMap[phaseName];
+
+                            // Флаг для отслеживания наличия компонентов в фазе
+                bool phaseHasComponents = false;
+
+                            // Флаг для отслеживания наличия хотя бы одного ненулевого компонента в фазе
+                bool phaseHasNonZeroComponent = false;
+
+                            // Чтение компонентов фазы
+                while (!(xmlReader.tokenType() == QXmlStreamReader::EndElement && xmlReader.name() == "Phase")) {
+                    if (xmlReader.tokenType() == QXmlStreamReader::StartElement) {
+                        QString elementType = xmlReader.name().toString();
+
+                                    // Обработка компонентов (Resistor, Inductor, Capacitor)
+                        if (elementType == "Resistor" || elementType == "Inductor" || elementType == "Capacitor") {
+                            phaseHasComponents = true;
+
+                            Component component;
+
+                                        // Получение атрибутов компонента
+                            QXmlStreamAttributes componentAttributes = xmlReader.attributes();
+                            component.name = componentAttributes.value("name").toString();
+                            QString resistanceString = componentAttributes.value("resistance").toString();
+                            bool ok;
+                            component.resistance = resistanceString.toDouble(&ok);
+
+                                        // Проверка успешности преобразования строки в число
+                            if (!ok) {
+                                qDebug() << "Error converting string to number: " << resistanceString;
+                                xmlReader.readNext();  // Пропускаем компонент с ошибкой и читаем следующий элемент
+                                continue;
+                            }
+
+                                        // Проверка на отрицательное сопротивление
+                            if (component.resistance < 0) {
+                                qDebug() << "Skipping component " << component.name << " due to negative resistance.";
+                                xmlReader.readNext();  // Пропускаем компонент с отрицательным сопротивлением и читаем следующий элемент
+                                continue;
+                            }
+
+                                        // Проверка наличия ненулевого сопротивления
+                            if (component.resistance != 0) {
+                                phaseHasNonZeroComponent = true;
+                            }
+
+                                        // Добавление компонента в соответствующий список фазы
+                            if (elementType == "Resistor") {
+                                phase.resistors.append(component);
+                            } else if (elementType == "Inductor") {
+                                phase.inductors.append(component);
+                            } else if (elementType == "Capacitor") {
+                                phase.capacitors.append(component);
+                            }
+                        }
+                     }
+
+                 xmlReader.readNext();
+                 }
+
+                            // Проверка наличия компонентов в фазе и отсутствия ошибок
+                if (!phaseHasComponents || !phaseHasNonZeroComponent) {
+                    qDebug() << "Skipping phase " << phaseName << " due to absence of components or all components have zero resistance.";
+                    phasesMap.remove(phaseName);  // Удаление фазы из карты
+                }
+
+            }
         }
     }
 }
